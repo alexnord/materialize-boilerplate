a85fa13af3ca039cea7a86fea8934170062f7512
Alex Nordlinger <alex.nordlinger@gmail.com>
2019-04-28 15:53:25 -0700

    Added login tests.

--- tests/Unit/AuthTest.php
+++ tests/Unit/AuthTest.php
@@ -8,12 +8,38 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class AuthTest extends TestCase
 {
     /**
-     * A basic test example.
+     * Test a successful login
      *
      * @return void
      */
     public function testCanLogin()
     {
-        $this->assertTrue(true);
+        $response = $this->post('api/auth/login', [
+            'email' => 'admin@test.com',
+            'password' => 'password',
+        ]);
+
+        $response->assertJsonStructure([
+            'access_token',
+            'token_type',
+            'expires_in',
+        ]);
+    }
+
+    /**
+     * Test a failed login with bad credentials.
+     *
+     * @return void
+     */
+    public function testFailedLogin()
+    {
+        $response = $this->post('api/auth/login', [
+            'email' => 'admin@test.com',
+            'password' => 'badpassword',
+        ]);
+
+        $response
+            ->assertStatus(401)
+            ->assertJson(['error' => 'Unauthorized']);
     }
 }
