905c12a687964b9da5e5c6c6c56f3acb5bfe2edd
Alex Nordlinger <alex.nordlinger@gmail.com>
2019-04-28 11:37:07 -0700

    Added GraphQL, users seeder, and the Users endpoint.

---
+++ app/GraphQL/Query/UsersQuery.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\GraphQL\Query;
+
+use App\Models\User;
+use GraphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Facades\GraphQL;
+use Rebing\GraphQL\Support\Query;
+use Rebing\GraphQL\Support\SelectFields;
+
+class UsersQuery extends Query
+{
+    protected $attributes = [
+        'name' => 'Users Query',
+        'description' => 'A query of users'
+    ];
+
+    public function type()
+    {
+        // result of query with pagination laravel
+        return Type::listOf(GraphQL::type('users'));
+    }
+
+    // arguments to filter query
+    public function args()
+    {
+        return [
+            'id' => [
+                'name' => 'id',
+                'type' => Type::int()
+            ],
+            'email' => [
+                'name' => 'email',
+                'type' => Type::string()
+            ]
+        ];
+    }
+
+    public function resolve($root, $args, SelectFields $fields)
+    {
+        $where = function ($query) use ($args) {
+            if (isset($args['id'])) {
+                $query->where('id', $args['id']);
+            }
+            if (isset($args['email'])) {
+                $query->where('email', $args['email']);
+            }
+        };
+
+        $user = User::with(array_keys($fields->getRelations()))
+            ->where($where)
+            ->select($fields->getSelect())
+            ->paginate();
+
+        return $user;
+    }
+}
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Testy McTesterson',
+            'email' => 'admin@test.com',
+            'password' => Hash::make('password'),
+        ]);
+    }
+}
